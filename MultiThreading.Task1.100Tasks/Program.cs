10ce51b04600d24be6e6197cc51bc57f97febf42
Yahor Zabotsin <56268079+zabotinegor@users.noreply.github.com>
2022-08-18 17:44:12 +0200

    100 Tasks (#1)
    Co-authored-by: Yahor Zabotsin <Yahor_Zabotsin@epam.com>

--- MultiThreading.Task1.100Tasks/Program.cs
+++ MultiThreading.Task1.100Tasks/Program.cs
@@ -3,10 +3,11 @@
  * Each Task should iterate from 1 to 1000 and print into the console the following string:
  * “Task #0 – {iteration number}”.
  */
-using System;
-
 namespace MultiThreading.Task1._100Tasks
 {
+    using System;
+    using System.Threading.Tasks;
+
     class Program
     {
         const int TaskAmount = 100;
@@ -19,7 +20,7 @@ namespace MultiThreading.Task1._100Tasks
             Console.WriteLine("Each Task should iterate from 1 to 1000 and print into the console the following string:");
             Console.WriteLine("“Task #0 – {iteration number}”.");
             Console.WriteLine();
-            
+
             HundredTasks();
 
             Console.ReadLine();
@@ -28,6 +29,20 @@ namespace MultiThreading.Task1._100Tasks
         static void HundredTasks()
         {
             // feel free to add your code here
+            var taskArray = new Task[TaskAmount];
+
+            for (var i = 0; i < TaskAmount; i++)
+            {
+                taskArray[i] = Task.Factory.StartNew(() =>
+                {
+                    for (var j = 0; j < MaxIterationsCount; j++)
+                    {
+                        Output(Task.CurrentId ?? -1, j);
+                    }
+                });
+            }
+
+            Task.WhenAll(taskArray);
         }
 
         static void Output(int taskNumber, int iterationNumber)
