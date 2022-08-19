1f5e0fda35d7dc75b03336aadb98a0177c4cc9a7
Yahor Zabotsin <Yahor_Zabotsin@epam.com>
2022-08-19 15:11:14 +0200

    Chaining

--- MultiThreading.Task2.Chaining/Program.cs
+++ MultiThreading.Task2.Chaining/Program.cs
@@ -5,12 +5,16 @@
  * Third Task – sorts this array by ascending.
  * Fourth Task – calculates the average value. All this tasks should print the values to console.
  */
-using System;
-
 namespace MultiThreading.Task2.Chaining
 {
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+
     class Program
     {
+        static readonly Random random = new Random();
+
         static void Main(string[] args)
         {
             Console.WriteLine(".Net Mentoring Program. MultiThreading V1 ");
@@ -23,7 +27,73 @@ namespace MultiThreading.Task2.Chaining
 
             // feel free to add your code
 
-            Console.ReadLine();
+            var array = new int[10];
+
+            Task.Run(async () =>
+            {
+                await Fill(array);
+                await Multiplay(array);
+                await Sort(array);
+                await Avarage(array);
+            });
+
+            Console.ReadKey();
+        }
+
+        private static Task Avarage(int[] array)
+        {
+            return Task.Run(() =>
+            {
+                Console.WriteLine("Avarage = " + array.DefaultIfEmpty(0).Average());
+            });
+        }
+
+        private static Task Sort(int[] array)
+        {
+            return Task.Run(() =>
+            {
+                Array.Sort(array);
+
+                Console.WriteLine("Sorted array: " + string.Join("; ", array));
+            });
+        }
+
+        private static Task Multiplay(int[] array)
+        {
+            return Task.Run(() =>
+            {
+                var x = GetRandom();
+
+                Console.WriteLine($"Multiplay value: {x}");
+
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] *= x;
+                }
+
+                Console.WriteLine(string.Join("; ", array));
+            });
+        }
+
+        private static int GetRandom()
+        {
+            lock (random)
+            {
+                return random.Next(1, 20);
+            }
+        }
+
+        private static Task Fill(int[] array)
+        {
+            return Task.Run(() =>
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] = GetRandom();
+                }
+
+                Console.WriteLine("Array: " + string.Join("; ", array));
+            });
         }
     }
 }
