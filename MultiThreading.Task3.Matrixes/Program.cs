f3d2a23f9d8d5776e5bdc6242561c0760b8d1c75
Yahor Zabotsin <zabotisneger@mail.ru>
2022-08-22 15:48:00 +0200

    added parallel matrix multiplication; a test for finding the size of a matrix, in which parallel multiplication is faster than usual

--- MultiThreading.Task3.Matrixes/Program.cs
+++ MultiThreading.Task3.Matrixes/Program.cs
@@ -19,7 +19,7 @@ namespace MultiThreading.Task3.MatrixMultiplier
             Console.WriteLine("3.	Write a program, which multiplies two matrices and uses class Parallel. ");
             Console.WriteLine();
 
-            const byte matrixSize = 7; // todo: use any number you like or enter from console
+            const byte matrixSize = 3; // todo: use any number you like or enter from console
             CreateAndProcessMatrices(matrixSize);
             Console.ReadLine();
         }
@@ -27,8 +27,8 @@ namespace MultiThreading.Task3.MatrixMultiplier
         private static void CreateAndProcessMatrices(byte sizeOfMatrix)
         {
             Console.WriteLine("Multiplying...");
-            var firstMatrix = new Matrix(sizeOfMatrix, sizeOfMatrix);
-            var secondMatrix = new Matrix(sizeOfMatrix, sizeOfMatrix);
+            var firstMatrix = new Matrix(sizeOfMatrix, sizeOfMatrix, true);
+            var secondMatrix = new Matrix(sizeOfMatrix, sizeOfMatrix, true);
 
             IMatrix resultMatrix = new MatricesMultiplier().Multiply(firstMatrix, secondMatrix);
 
