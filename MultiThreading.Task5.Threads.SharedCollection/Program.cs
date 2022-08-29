b7dfc81ebe73f2c25bc31c4a50e06002791e5e39
Yahor Zabotsin <56268079+zabotinegor@users.noreply.github.com>
2022-08-29 17:36:40 +0200

    SharedCollection (#4)
    * draft solution
    
    * SharedCollection task
    
    Co-authored-by: Yahor Zabotsin <zabotisneger@mail.ru>

--- MultiThreading.Task5.Threads.SharedCollection/Program.cs
+++ MultiThreading.Task5.Threads.SharedCollection/Program.cs
@@ -5,11 +5,17 @@
  * Use Thread, ThreadPool or Task classes for thread creation and any kind of synchronization constructions.
  */
 using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace MultiThreading.Task5.Threads.SharedCollection
 {
     class Program
     {
+        static readonly Random random = new Random();
+        static readonly Semaphore semaphore = new Semaphore(1, 1);
+
         static void Main(string[] args)
         {
             Console.WriteLine("5. Write a program which creates two threads and a shared collection:");
@@ -18,8 +24,64 @@ namespace MultiThreading.Task5.Threads.SharedCollection
             Console.WriteLine();
 
             // feel free to add your code
+            var blockingCollection = new BlockingCollection<int>();
+            var isChanged = false;
+            var resetEvent = new ManualResetEvent(false);
 
-            Console.ReadLine();
+            var creationTask = new Task(() =>
+            {
+                var count = GetRandom(10, 20);
+                for (int i = 0; i < count;)
+                {
+                    semaphore.WaitOne();
+
+                    if (!isChanged)
+                    {
+                        Console.WriteLine($"Task {Task.CurrentId} adding item");
+                        isChanged = blockingCollection.TryAdd(GetRandom(1, 100));
+                        i++;
+                    }
+
+                    semaphore.Release();
+                }
+
+                blockingCollection.CompleteAdding();
+                resetEvent.Set();
+            });
+
+            var printingTask = new Task(() =>
+            {
+                while (!blockingCollection.IsAddingCompleted)
+                {
+                    semaphore.WaitOne();
+
+                    if (isChanged)
+                    {
+                        Console.WriteLine($"Task {Task.CurrentId}: " + string.Join("; ", blockingCollection));
+                        isChanged = false;
+                        Task.Delay(GetRandom(1, 2) * 1000);
+                    }
+
+                    semaphore.Release();
+                }
+            });
+
+            printingTask.Start();
+            creationTask.Start();
+
+            resetEvent.WaitOne();
+
+            Console.WriteLine("Program finished!");
+            Console.ReadKey();
+        }
+
+        private static int GetRandom(int v1, int v2)
+        {
+            lock (random)
+            {
+                var result = random.Next(v1, v2);
+                return result;
+            }
         }
     }
 }
