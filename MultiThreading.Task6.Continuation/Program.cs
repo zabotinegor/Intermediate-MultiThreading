2fe15e2cc13cdfc3302bbbb4d4334febb9e5a917
Yahor Zabotsin <zabotisneger@mail.ru>
2022-09-01 11:39:34 +0200

    Continuation (options a, d)

--- MultiThreading.Task6.Continuation/Program.cs
+++ MultiThreading.Task6.Continuation/Program.cs
@@ -7,6 +7,8 @@
    Demonstrate the work of the each case with console utility.
 */
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace MultiThreading.Task6.Continuation
 {
@@ -24,7 +26,58 @@ namespace MultiThreading.Task6.Continuation
 
             // feel free to add your code
 
-            Console.ReadLine();
+            Console.WriteLine("Option a");
+            var taskA = Task.Run(() => DateTime.Today.DayOfWeek);
+            var taskB = taskA.ContinueWith(antecedent => Console.WriteLine($"Today is {antecedent.Result}."));
+
+            Task.WaitAll(taskA, taskB);
+
+            // todo: implement
+            Console.WriteLine("Option b");
+
+            // todo: implement
+            Console.WriteLine("Option c");
+
+            Console.WriteLine("Option d");
+
+            using var cts = new CancellationTokenSource();
+            var token = cts.Token;
+            var timer = new Timer(Elapsed, cts, 5000, Timeout.Infinite);
+
+            var taskC = Task.Run(() => 
+            {
+                Console.WriteLine($"Task C started");
+
+                for (int i = 0; ; i++)
+                {
+                    if (token.IsCancellationRequested)
+                    {
+                        Console.WriteLine("Cancellation requested in antecedent...");
+                        token.ThrowIfCancellationRequested();
+                    }
+
+                    Task.Delay(1000);
+                }
+            }, token);
+            // not outside of threadpool
+            var taskD = taskC.ContinueWith(antecedent => 
+            {
+                Console.WriteLine("Task D started");
+            }, TaskContinuationOptions.OnlyOnCanceled);
+
+            Task.WaitAny(taskD);
+
+            Console.WriteLine("Program finished");
+            Console.ReadKey();
+        }
+
+        private static void Elapsed(object state)
+        {
+            if (state is CancellationTokenSource cts)
+            {
+                Console.WriteLine("Cancellation request issued...");
+                cts.Cancel();                
+            }
         }
     }
 }
